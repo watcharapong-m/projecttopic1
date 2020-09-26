373ab883c5f50104ecd09e1984a70c3deb09e467
fame <chutipong.sa.60@ubu.ac.th>
2020-09-26 17:21:27 +0700

    add mean

---
+++ mean/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+import { MainPageComponent } from './main-page/main-page.component';
+import { ReviewComponent } from './review/review.component';
+import { DescriptionComponent } from './description/description.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AuthGuard } from './shared/guards';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   component: HomeComponent,
+  //   canActivate: [AuthGuard],
+  // },
+  {
+    path: '',
+    component: MainPageComponent,
+  },
+  {
+    path: 'description',
+    component: DescriptionComponent,
+  },
+  {
+    path: 'review',
+    component: ReviewComponent,
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ mean/src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule, APP_INITIALIZER } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { SharedModule } from './shared/shared.module';
+
+import { AppComponent } from './app.component';
+import { AuthHeaderInterceptor } from './interceptors/header.interceptor';
+import { CatchErrorInterceptor } from './interceptors/http-error.interceptor';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AuthService } from './shared/services';
+import { DescriptionComponent } from './description/description.component';
+import { ReviewComponent } from './review/review.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+export function appInitializerFactory(authService: AuthService) {
+  return () => authService.checkTheUserOnTheFirstLoad();
+}
+
+@NgModule({
+  imports: [BrowserAnimationsModule, HttpClientModule, SharedModule, AppRoutingModule],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, DescriptionComponent, ReviewComponent, MainPageComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthHeaderInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CatchErrorInterceptor,
+      multi: true,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: appInitializerFactory,
+      multi: true,
+      deps: [AuthService],
+    },
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
